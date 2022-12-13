fb4a5bde43800e80e4f4b709e508f0126d921dd6
Rajesh kumar <113416194+rajeshkumar997@users.noreply.github.com>
2022-12-13 14:12:50 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,8 @@
 import React,{useState,useEffect} from 'react'
+import Result from './Result';
 import '../styles/App.css';
+
 const App = () => {
 
-  return (
-    <div id="main">
-      <input id='input1' onChange={}/>
-       +
-      <input id='input2' onChange={}/> 
-      
-      <p id='result'></p>
-    </div>
-  )
-}
-
-
-export default App;
+  const [input1, setInput1] = useState("")
+  const [input2, setInput2] = useState("")
