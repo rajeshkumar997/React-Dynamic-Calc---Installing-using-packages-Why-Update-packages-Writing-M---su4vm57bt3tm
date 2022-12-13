1f93d4afae0cd3de86447a7dd66cb5c292a5092a
Rajesh kumar <113416194+rajeshkumar997@users.noreply.github.com>
2022-12-13 14:13:39 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -6,3 +6,17 @@ const App = () => {
 
   const [input1, setInput1] = useState("")
   const [input2, setInput2] = useState("")
+return (
+    <div id="main">
+      <input id='input1' onChange={(e) => setInput1(e.target.value)}/>
+       +
+       <input id='input2' onChange={(e) => setInput2(e.target.value)}/> 
+
+      {/* <Result num1 = {input1}  num2 = {input2}/> */}
+      <Result num1 = {Number(input1)}  num2 = {Number(input2)}/>
+    </div>
+  )
+}
+
+
+export default App;
