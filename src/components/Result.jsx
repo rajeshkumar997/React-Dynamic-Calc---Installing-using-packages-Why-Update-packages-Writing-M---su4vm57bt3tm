c9c50e897416476782d057bba1e3fa9265e398dd
Rajesh kumar <113416194+rajeshkumar997@users.noreply.github.com>
2022-12-13 14:15:27 +0530

    Create Result.jsx

---
+++ src/components/Result.jsx
@@ -0,0 +1,20 @@
+import React,{useState,useEffect} from 'react'
+import '../styles/App.css';
+
+const Result = (props) => {
+
+    const {num1, num2} = props;//destructuring
+    return(
+        <p id='result'>{Number(props.num1) + Number(props.num2)}</p>
+        <p id='result'>{num1 + num2}</p>//destructuring
+    )
+}
+const Result = ({num1,num2}) => {//simply I destructure here
+
+    return(
+        <p id='result'>{Number(props.num1) + Number(props.num2)}</p>
+        <p id='result'>{num1 + num2}</p>//destructuring
+    )
+}
+
+export default Result;
